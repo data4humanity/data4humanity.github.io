3d0575c23c029fc5e0e54b72290df0056bcf1883
Kevin Hu <kevinhuwest@gmail.com>
2020-09-14 14:50:56 -0400

    Add about page layout

--- src/pages/about.js
+++ src/pages/about.js
@@ -7,7 +7,28 @@ import SEO from "../components/seo"
 const About = () => (
 	<Layout>
 		<SEO title="About" />
-		<div>This is about!</div>
+		<div className="pt-16 pb-4 px-12 relative overflow-hidden">
+			<div className="text-5xl font-semibold bg-white max-w-full text-center md:text-left leading-tight">
+				About us
+			</div>
+		</div>
+		<div className="pt-4 pb-4 px-12 relative overflow-hidden">
+			<div className="text-4xl font-semibold bg-white max-w-full text-center md:text-left leading-tight">
+				What we do
+			</div>
+		</div>
+		<div className="pt-4 pb-4 px-12 relative overflow-hidden">
+			<div className="text-4xl font-semibold bg-white max-w-full text-center md:text-left leading-tight">
+				Our members
+			</div>
+			<div className="text-3xl font-semibold bg-white max-w-full text-center md:text-left leading-tight">
+				Leadership
+			</div>
+			<div className="text-3xl font-semibold bg-white max-w-full text-center md:text-left leading-tight">
+				Project teams
+			</div>
+			Coming soon!
+		</div>
 	</Layout>
 )
 
